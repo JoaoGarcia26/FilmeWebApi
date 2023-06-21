c3b70d0a52b589fe81db17dbd508dd0d4e52d45f
João Victor Araujo Garcia <joaovictor.a.garcia@gmail.com>
2023-06-21 16:07:19 -0300

    Projeto final documentado

---
+++ Data/Dtos/CreateFilmeDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Data.Dtos;
+
+public class CreateFilmeDto
+{ 
+    [Required(ErrorMessage = "O título do filme é obrigatório")]
+    public string Titulo { get; set; }
+    [Required(ErrorMessage = "O genero do filme é obrigatório")]
+    [StringLength(50, ErrorMessage = "O tamanho do genero não pode exceder 50 caracteres")]
+    public string Genero { get; set; }
+    [Required(ErrorMessage = "O diretor do filme é obrigatório")]
+    public string Diretor { get; set; }
+    [Required(ErrorMessage = "O valor da duração não é valido")]
+    [Range(1, 360, ErrorMessage = "A duração deve ser entre 1 a 360 minutos")]
+    public int Duracao { get; set; }
+}
